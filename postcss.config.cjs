8b950b28171542bea2e32fcb44eb9b1aa4a4a21a
l30047414 <luojunjie19@huawei.com>
2024-03-25 15:08:41 +0800

    feat: 为Button添加样式

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 /* eslint-env node */
 module.exports = {
   plugins: [
-    require('postcss-nested')
+    require('postcss-nested'),
+    require('postcss-each')
   ]
 }
--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <a href="">link</a>
-  <Button type="primary" disabled plain ref="buttonRef">Test</Button>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
   </header>
+
+  <main>
+    <Button ref="buttonRef">Test Button</Button>
+    <Button plain>Plain Button</Button>
+    <Button round>Round Button</Button>
+    <Button circle>VK</Button>
+    <Button disabled>Disabled Button</Button><br/><br/>
+    <Button type="primary">Primary</Button>
+    <Button type="success">Success</Button>
+    <Button type="info">Info</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="danger">Danger</Button><br/><br/>
+    <Button type="primary" plain>Primary</Button>
+    <Button type="success" plain>Success</Button>
+    <Button type="info" plain>Info</Button>
+    <Button type="warning" plain>Warning</Button>
+    <Button type="danger" plain>Danger</Button><br/><br/>
+    <Button size="large">Large</Button>
+    <Button size="small">Small</Button><br/><br/>
+  </main>
 </template>
