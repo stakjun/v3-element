a139555d97aba1518366dcbda6e087814fcc1800
luojunjie <578715385@qq.com>
2024-04-14 11:56:23 +0800

    fix: button文档修改

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,43 +1,46 @@
-import { defineConfig } from 'vitepress'
-import { containerPreview, componentPreview } from '@vitepress-demo-preview/plugin'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitepress';
+import {
+	containerPreview,
+	componentPreview
+} from '@vitepress-demo-preview/plugin';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "V3 Element",
-  description: "基于 Vue3 的组件库",
-  base: '/v3-element/',
-  vite: {
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('../../src', import.meta.url))
-      }
-    }
-  },
-  markdown: {
-    config(md) {
-      md.use(containerPreview)
-      md.use(componentPreview)
-    }
-  },
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: '/img/logo.jpg',
-    nav: [
-      { text: '安装', link: '/components/' },
-      { text: '组件', link: '/components/button' }
-    ],
-    sidebar: [
-      {
-        text: 'Basic 基础组件',
-        items: [
-          { text: 'Button 按钮', link: '/components/button' }
-        ]
-      },
-    ],
+	title: 'V3 Element',
+	description: '基于 Vue3 的组件库',
+	base: '/v3-element/',
+	vite: {
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('../../src', import.meta.url))
+			}
+		}
+	},
+	markdown: {
+		config(md) {
+			md.use(containerPreview);
+			md.use(componentPreview);
+		}
+	},
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		logo: '/img/logo.jpg',
+		nav: [
+			{ text: '安装', link: '/components/' },
+			{ text: '组件', link: '/components/button' }
+		],
+		sidebar: [
+			{
+				text: 'Basic 基础组件',
+				items: [{ text: 'Button 按钮', link: '/components/button' }]
+			}
+		],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/stakjun/v3-element' }
-    ]
-  }
-})
+		socialLinks: [
+			{ icon: 'github', link: 'https://github.com/stakjun/v3-element' }
+		],
+
+		outline: [2, 3]
+	}
+});
