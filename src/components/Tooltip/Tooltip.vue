b2656bae2544b95953e1a525147c3116d073c858
luojunjie <578715385@qq.com>
2024-04-20 16:50:40 +0800

    feat: Tooltip-支持click和hover动态事件绑定

--- src/components/Tooltip/Tooltip.vue
+++ src/components/Tooltip/Tooltip.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="vk-tooltip">
-    <div class="vk-tooltip__trigger" ref="triggerNode" @click="togglePopper">
+  <div class="vk-tooltip" v-on="outerEvents">
+    <div class="vk-tooltip__trigger" ref="triggerNode" v-on="events">
       <slot />
     </div>
     <div class="vk-tooltip__popper" ref="popperNode" v-if="isOpen">
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { reactive, ref, watch } from 'vue';
 import type { TooltipEmits, TooltipProps } from './types';
 import { createPopper, type Instance } from '@popperjs/core';
 
@@ -19,7 +19,8 @@ defineOptions({
 });
 
 const props = withDefaults(defineProps<TooltipProps>(), {
-  placement: 'right'
+  placement: 'bottom',
+  trigger: 'hover'
 });
 const emits = defineEmits<TooltipEmits>();
 
@@ -29,14 +30,37 @@ const isOpen = ref(false);
 const triggerNode = ref<HTMLElement | null>(null);
 /** 内容节点 */
 const popperNode = ref<HTMLElement | null>(null);
-
+/** popper 实例 */
 let popperInstance: null | Instance = null;
+
+/** 打开 */
+const open = () => {
+  isOpen.value = true;
+};
+/** 关闭 */
+const close = () => {
+  isOpen.value = false;
+};
 
 /** 点击 trigger */
 const togglePopper = () => {
   isOpen.value = !isOpen.value;
   emits('visible-change', isOpen.value);
 };
+
+/** 动态绑定事件 */
+let events: Record<string, any> = reactive({});
+let outerEvents: Record<string, any> = reactive({});
+/** 绑定事件 */
+const attachEvents = () => {
+  if (props.trigger === 'hover') {
+    events['mouseenter'] = open;
+    outerEvents['mouseleave'] = close;
+  } else {
+    events['click'] = togglePopper;
+  }
+};
+attachEvents();
 
 watch(
   isOpen,
@@ -52,5 +76,16 @@ watch(
     }
   },
   { flush: 'post' }
+);
+
+watch(
+  () => props.trigger,
+  (newTrigger, oldTrigger) => {
+    if (newTrigger !== oldTrigger) {
+      events = {};
+      outerEvents = {};
+      attachEvents();
+    }
+  }
 );
 </script>
