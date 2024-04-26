e16c11088141069a33224b2a974c523be9d8e074
luojunjie <578715385@qq.com>
2024-04-26 12:08:41 +0800

    feat: 支持 清空输入值

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -23,14 +23,23 @@
           <slot name="prefix" />
         </span>
         <input
+          v-model="innerValue"
           :type="type"
           :disabled="disabled"
           class="vk-input__inner"
           @input="handleInput"
+          @focus="handleFocus"
+          @blur="handleBlur"
         />
         <!-- suffix slot -->
-        <span class="vk-input__suffix" v-if="$slots.suffix">
+        <span class="vk-input__suffix" v-if="$slots.suffix || showClear">
           <slot name="suffix" />
+          <Icon
+            v-if="showClear"
+            icon="circle-xmark"
+            class="vk-input__clear"
+            @click="clear"
+          />
         </span>
       </div>
       <!-- append slot -->
@@ -41,17 +50,21 @@
     <!-- textarea -->
     <template v-else>
       <textarea
+        v-model="innerValue"
         class="vk-textarea__wrapper"
         :disabled="disabled"
         @input="handleInput"
+        @focus="handleFocus"
+        @blur="handleBlur"
       />
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import type { InputEmits, InputProps } from './types';
+import Icon from '../Icon';
 
 defineOptions({
   name: 'VkInput'
@@ -62,9 +75,27 @@ const props = withDefaults(defineProps<InputProps>(), {
 });
 const emits = defineEmits<InputEmits>();
 
+/** 输入框的值 */
 const innerValue = ref(props.modelValue);
+/** 是否聚焦 */
+const isFocus = ref(false);
+/** 展示清除按钮 */
+const showClear = computed(
+  () => props.clearable && !props.disabled && innerValue.value && isFocus.value
+);
 
 const handleInput = () => {
+  emits('update:modelValue', innerValue.value);
+};
+const handleFocus = () => {
+  isFocus.value = true;
+};
+const handleBlur = () => {
+  isFocus.value = false;
+};
+/** 清除 */
+const clear = () => {
+  innerValue.value = '';
   emits('update:modelValue', innerValue.value);
 };
 
