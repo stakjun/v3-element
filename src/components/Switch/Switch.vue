d26953ec8051efdd98a31b59f84f843b2fcec695
luojunjie <578715385@qq.com>
2024-04-27 12:50:20 +0800

    feat: Switch 组件基本实现

---
+++ src/components/Switch/Switch.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="vk-switch"
+    :class="{
+      [`vk-switch--${size}`]: size,
+      'is-disabled': disabled,
+      'is-checked': checked
+    }"
+  >
+    <input
+      class="vk-switch__input"
+      type="checkbox"
+      role="switch"
+      :name="name"
+      :disabled="disabled"
+    />
+    <div class="vk-switch__core">
+      <div class="vk-switch__core-action"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import type { SwitchEmits, SwitchProps } from './types';
+
+defineOptions({
+  name: 'VkSwitch',
+  inheritAttrs: false
+});
+
+const props = defineProps<SwitchProps>();
+const emits = defineEmits<SwitchEmits>();
+
+const innerValue = ref(props.modelValue);
+const checked = computed(() => innerValue.value);
+</script>
---
+++ src/components/Switch/types.ts
@@ -0,0 +1,21 @@
+export interface SwitchProps {
+  /** 绑定值，必须等于 active-value 或 inactive-value，默认为 Boolean 类型 */
+  modelValue: string;
+  /** switch 的大小 */
+  size?: 'small' | 'large';
+  /** 是否禁用 */
+  disabled?: boolean;
+  /** switch 打开时的文字描述 */
+  activeText?: string;
+  /** switch 的状态为 off 时的文字描述 */
+  inactiveText?: string;
+  /** switch 对应的 name 属性 */
+  name?: string;
+  /** input 的 id */
+  id?: string;
+}
+
+export interface SwitchEmits {
+  (e: 'change', value: boolean): void;
+  (e: 'update:modelValue', value: boolean): void;
+}
