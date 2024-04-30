226dd545c5bd6f5c38c530ea89c5d208094dd478
luojunjie <578715385@qq.com>
2024-04-30 21:56:03 +0800

    feat: 添加数据和规则

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -5,7 +5,11 @@
 </template>
 
 <script setup lang="ts">
+import type { FormProps } from './types';
+
 defineOptions({
   name: 'VkForm'
 });
+
+defineProps<FormProps>();
 </script>
--- src/components/Form/types.ts
+++ src/components/Form/types.ts
@@ -1,3 +1,8 @@
+export interface FormProps {
+  model: Record<string, any>;
+  rules: Record<string, any>;
+}
+
 export interface FormItemProps {
   label: string;
 }
